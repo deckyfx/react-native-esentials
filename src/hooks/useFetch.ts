2dd81be0fc0409bbf62b83c15e134abd74d25a68
Decky Fiyemonda <decky.fiyemonda@shark.tech>
2022-12-16 15:09:13 +0700

    v1.0.12a

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -8,14 +8,24 @@ export interface FetchState<T> {
   loading?: boolean;
 }
 
-export interface FectOutput<T> extends FetchState<T> {
-  execute: () => void;
+export enum DispatchFecthState {
+  LOADING = 'loading',
+  FETCHED = 'fetched',
+  ERROR = 'error',
+  IDLE = 'idle',
 }
 
 type Cache<T> = { [url: string]: T };
 
 // discriminated union type
-type Action<T> = { type: 'loading' } | { type: 'fetched'; payload: T } | { type: 'error'; payload: Error };
+type Action<T> =
+  | { type: DispatchFecthState.LOADING }
+  | { type: DispatchFecthState.FETCHED; payload: T }
+  | { type: DispatchFecthState.ERROR; payload: Error };
+
+export interface FectOutput<T> extends FetchState<T> {
+  execute: (url: string | undefined, options: RequestInit | undefined) => void;
+}
 
 const useFetch = <T = unknown>(url?: string, options?: RequestInit, autorun: boolean = true): FectOutput<T> => {
   const cache = useRef<Cache<T>>({});
@@ -32,11 +42,11 @@ const useFetch = <T = unknown>(url?: string, options?: RequestInit, autorun: boo
   // Keep state logic separated
   const fetchReducer = (_state: FetchState<T>, action: Action<T>): FetchState<T> => {
     switch (action.type) {
-      case 'loading':
+      case DispatchFecthState.LOADING:
         return { ...initialState, loading: true };
-      case 'fetched':
+      case DispatchFecthState.FETCHED:
         return { ...initialState, loading: false, data: action.payload };
-      case 'error':
+      case DispatchFecthState.ERROR:
         return { ...initialState, loading: false, error: action.payload };
       default:
         return _state;
@@ -45,52 +55,63 @@ const useFetch = <T = unknown>(url?: string, options?: RequestInit, autorun: boo
 
   const [state, dispatch] = useReducer(fetchReducer, initialState);
 
-  const execute = useCallback(async () => {
-    // Do nothing if the url is not given
-    if (!url) return;
-
-    cancelRequest.current = false;
-
-    const fetchData = async () => {
-      dispatch({ type: 'loading' });
-
-      // If a cache exists for this url, return it
-      if (cache.current[url]) {
-        dispatch({ type: 'fetched', payload: cache.current[url] });
-        return;
+  const execute = useCallback(
+    async (newurl: string | null | undefined, newoptions: RequestInit | null | undefined) => {
+      if (!newurl) {
+        newurl = url;
       }
 
-      try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-          throw new Error(response.statusText);
+      if (!newoptions) {
+        newoptions = options;
+      }
+
+      // Do nothing if the url is not given
+      if (!newurl) return;
+
+      cancelRequest.current = false;
+
+      const fetchData = async () => {
+        dispatch({ type: DispatchFecthState.LOADING });
+
+        // If a cache exists for this url, return it
+        if (cache.current[newurl!]) {
+          dispatch({ type: DispatchFecthState.FETCHED, payload: cache.current[newurl!] });
+          return;
         }
 
-        const data = (await response.json()) as T;
-        cache.current[url] = data;
-        if (cancelRequest.current) return;
+        try {
+          const response = await fetch(newurl!, newoptions!);
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
 
-        dispatch({ type: 'fetched', payload: data });
-      } catch (error) {
-        if (cancelRequest.current) return;
+          const data = (await response.json()) as T;
+          cache.current[newurl!] = data;
+          if (cancelRequest.current) return;
 
-        dispatch({ type: 'error', payload: error as Error });
-      }
-    };
+          dispatch({ type: DispatchFecthState.FETCHED, payload: data });
+        } catch (error) {
+          if (cancelRequest.current) return;
 
-    void fetchData();
+          dispatch({ type: DispatchFecthState.ERROR, payload: error as Error });
+        }
+      };
 
-    // Use the cleanup function for avoiding a possibly...
-    // ...state update after the component was unmounted
-    return () => {
-      cancelRequest.current = true;
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [url]);
+      void fetchData();
+
+      // Use the cleanup function for avoiding a possibly...
+      // ...state update after the component was unmounted
+      return () => {
+        cancelRequest.current = true;
+      };
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    },
+    [url],
+  );
 
   useEffect(() => {
     if (autorun) {
-      execute();
+      execute(undefined, undefined);
     }
   }, [url]);
 
